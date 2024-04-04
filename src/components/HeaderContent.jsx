1ff5100cd95bac7a5c84059d841a2d39d27c94f7
Pankajsiwach123321 <pankusiwach000@gmai.com>
2024-04-04 14:10:22 +0530

    text change

--- src/components/HeaderContent.jsx
+++ src/components/HeaderContent.jsx
@@ -15,7 +15,7 @@ const HeaderContent = () => {
         <div className="max-w-584">
           <h1 className=" fw-light mb-3 outfit lh-3xl fs-xl c-primery">
             Inspiring Innovation, Elevating Solutions –{" "}
-            <span className=" fw-bold  lh-2xl">Noble Mind"</span>
+            <span className=" fs-lg fw-800  lh-2xl">Noble Mind"</span>
           </h1>
           <p className="exo mb-0 fw-normal fs-sm lh-s c-secandary-high">
             At Noble Mind is a forward-thinking AI specialised company dedicated
