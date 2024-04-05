2609af87152a290428b9c58ac8733cc0939fb2fd
Pankajsiwach123321 <pankusiwach000@gmai.com>
2024-04-05 10:14:23 +0530

    Subscribe verification

--- src/components/Subcribe.jsx
+++ src/components/Subcribe.jsx
@@ -4,28 +4,39 @@ import Button from "./Button";
 import { ArrowPloy } from "../comman/icon";
 
 const Subcribe = () => {
-  //   const [fromdata, setFromdata] = React.useState("");
-  //   const [Fromerror, setFromerror] = React.useState("");
-  //   const inputCon = (e) => {
-  //     const { name } = e.target;
-  //     setFromdata(name);
-  //   };
-  //   const clg = (e) => {
-  //     e.preventDefault();
-  //     const regex = {
-  //       email:
-  //         /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-  //     };
-  //     const error = {};
-  //     if (!regex.email.test(fromdata)) {
-  //       error.name = "invaild eamil";
-  //     }
-  //     setFromerror(error);
-  //     if (Object.keys(error).length === 0) {
-  //       setFromdata("");
-  //       setFromerror("");
-  //     }
-  //   };
+  const [formData, setFormData] = React.useState({
+    email: "",
+  });
+  const [formErrors, setFormErrors] = React.useState({
+    email: "",
+  });
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const regex = {
+      email: /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,})$/,
+    };
+    const errors = {};
+    if (!regex.email.test(formData.email)) {
+      errors.email = "email is invalid!!";
+    }
+    setFormErrors(errors);
+    if (Object.keys(errors).length === 0) {
+      setFormData({
+        email: "",
+      });
+      setFormErrors({
+        email: "",
+      });
+      alert("Newsletter! Subscribed");
+    }
+  };
   return (
     <div className=" position-relative z-1 mt-md-5 pt-lg-23" id="service">
       <Container>
@@ -44,20 +55,28 @@ const Subcribe = () => {
             Be the first to get exclusive offers and the latest news.
           </p>
           <div className="email-outer ">
-            <form className="d-flex justify-content-between align-items-center">
+            <form
+              onSubmit={handleSubmit}
+              className="d-flex justify-content-between align-items-center"
+            >
               <input
                 type="email"
+                onChange={handleChange}
+                value={formData.email}
+                name="email"
                 placeholder="Enter your email"
-                className="input-subcribe"
+                className="input-subcribe bg-transparent"
               />
               <input
-                onClick={(e)=>e.preventDefault()}
                 type="submit"
                 value="Subscribe"
                 className="subscribe-button transition-300 px-4 py-14 lh-s fw-semibold outfit c-tertiry"
               />
             </form>
           </div>
+          {formErrors.email && (
+            <p className=" text-center text-white pt-1 exo">{formErrors.email}</p>
+          )}
         </div>
       </Container>
       <span className="arrowsubscribe1">
